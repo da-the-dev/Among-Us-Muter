e08b344ca31dd8d291729f776dba1d6393e1fa02
da-the-dev <leshiktk@me.com>
2020-10-19 22:13:32 +0300

    Reverting back to stable state

--- index.js
+++ index.js
@@ -10,22 +10,6 @@ client.once('ready', () => {
     console.log("Im the Impostor!")
 })
 
-client.on('voiceStateUpdate', (oldUser, newUser) => {
-    const keyv = new Keyv(process.env.REDISCLOUD_URL)
-    var db = await keyv.get(newUser.guild.id)
-
-    // User left Among Us voicechannel
-    if(oldUser.channelID == oldUser.guild.channels.cache.find(channel => channel.id == db.voiceChannel)) {
-        newUser.setMute(false)
-        return
-    }
-    // User joined Among Us voicechannel
-    if(newUser.guild.channels.cache.find(channel => channel.id == db.voiceChannel) && db.isMuted) {
-        newUser.setMute(true)
-        return
-    }
-})
-
 client.on('message', async msg => {
     if(!msg.author.bot && msg.content[0] == prefix) {
         var args = msg.content.slice(1).split(" ")
