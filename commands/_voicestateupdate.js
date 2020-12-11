f4463e03bad2cebc62924d970dbdfc30edc8d7c7
da-the-dev <leshiktk@me.com>
2020-12-11 21:41:40 +0300

    Got stuff sorted out

--- commands/_voicestateupdate.js
+++ commands/_voicestateupdate.js
@@ -1,37 +1,62 @@
 const Discord = require('discord.js')
-const asyncRedis = require('async-redis');
+const exists = (v) => {
+    if(v)
+        return true
+    else
+        return false
+}
 module.exports =
     /**
-     * @param {Array<string>} guild Server guild
+     * @param {Discord.VoiceState} newState The voice state before the update
+     * @param {Discord.VoiceState} oldState The voice state after the update
     */
-    async (voiceState1, voiceState2) => {
-        if(voiceState1.channelID == voiceState2.channelID)
-            return
+    async (oldState, newState) => {
+        /**@type {Discord.VoiceChannel} */
+        let oldChannel = oldState.channel
+        /**@type {Discord.VoiceChannel} */
+        let newChannel = newState.channel
+        // console.log(exists(oldChannel), exists(newChannel))
 
-        const db = asyncRedis.createClient(process.env.REDISCLOUD_URL)
-        var data = JSON.parse(await db.get(voiceState1.guild.id))
-        if(!data)
-            data = JSON.parse(await db.get(voiceState2.guild.id))
+        // Connected
+        if(exists(oldChannel) && exists(newChannel)) {
+            var category = newState.guild.channels.cache.find(c => c.type == 'category' && c.name == 'Among Us rooms')
+            if(newChannel.parent == category) {
+                if(newChannel.members.size == 1) {
+                    newState.member.roles.add(newState.guild.roles.cache.find(r => r.name == 'AUM Muter Role'))
+                        .catch(e => { console.log(e); return })
+                }
+            }
+        }
 
-        if(!data)
-            db.quit()
-        if(!data.voiceChannel)
-            db.quit()
 
-        // If user leaves 
-        if(voiceState1.channelID == data.voiceChannel)
-            voiceState2.setMute(false)
 
-        // If user joins
-        if(voiceState2.channelID && voiceState2.channelID == data.voiceChannel)
-            if(data.isMuted)
-                voiceState2.setMute(true)
-            else
-                voiceState2.setMute(false)
+        // if(voiceState1.channelID == voiceState2.channelID)
+        //     return
 
-        // If user leaves completely
-        if(!voiceState2.channelID && voiceState1.channelID == data.voiceChannel)
-            voiceState1.setMute(false)
+        // const db = asyncRedis.createClient(process.env.REDISCLOUD_URL)
+        // var data = JSON.parse(await db.get(voiceState1.guild.id))
+        // if(!data)
+        //     data = JSON.parse(await db.get(voiceState2.guild.id))
 
-        db.quit()
+        // if(!data)
+        //     db.quit()
+        // if(!data.voiceChannel)
+        //     db.quit()
+
+        // // If user leaves 
+        // if(voiceState1.channelID == data.voiceChannel)
+        //     voiceState2.setMute(false)
+
+        // // If user joins
+        // if(voiceState2.channelID && voiceState2.channelID == data.voiceChannel)
+        //     if(data.isMuted)
+        //         voiceState2.setMute(true)
+        //     else
+        //         voiceState2.setMute(false)
+
+        // // If user leaves completely
+        // if(!voiceState2.channelID && voiceState1.channelID == data.voiceChannel)
+        //     voiceState1.setMute(false)
+
+        // db.quit()
     }
--- commands/amg.js
+++ commands/amg.js
@@ -5,54 +5,56 @@ module.exports =
      * @param {Array<string>} args Command argument
      * @param {Discord.Message} msg Discord message object
      * @param {Discord.Client} client Discord client object
+     * @description Usage: $amg <name>
     */
     async (args, msg, client) => {
-        const redis = asyncRedis.createClient(process.env.REDISCLOUD_URL)
-        var db = JSON.parse(await redis.get(msg.guild.id))
+        // if()
+        // const redis = asyncRedis.createClient(process.env.REDISCLOUD_URL)
+        // var db = JSON.parse(await redis.get(msg.guild.id))
 
-        if(!db) {
-            msg.reply(`couldn't find any data related to this server. Try \`${client.prefix}register\``)
-            redis.quit()
-            return
-        }
-        var roleId = db['muteRoleId']
-        if(!roleId) {
-            msg.reply(`no mute role specified. Try \`${client.prefix}addMuteRole @role\` first`)
-            redis.quit()
-            return
-        }
-        var voiceChannelId = db['voiceChannel']
-        if(!voiceChannelId) {
-            msg.reply(`no Among Us voicechannel specified. Try \`${client.prefix}addAmongUsChannel <channelid> first\``)
-            redis.quit()
-            return
-        }
+        // if(!db) {
+        //     msg.reply(`couldn't find any data related to this server. Try \`${client.prefix}register\``)
+        //     redis.quit()
+        //     return
+        // }
+        // var roleId = db['muteRoleId']
+        // if(!roleId) {
+        //     msg.reply(`no mute role specified. Try \`${client.prefix}addMuteRole @role\` first`)
+        //     redis.quit()
+        //     return
+        // }
+        // var voiceChannelId = db['voiceChannel']
+        // if(!voiceChannelId) {
+        //     msg.reply(`no Among Us voicechannel specified. Try \`${client.prefix}addAmongUsChannel <channelid> first\``)
+        //     redis.quit()
+        //     return
+        // }
 
-        if(msg.member.roles.cache.find(r => r.permissions.has('ADMINISTRATOR')) || msg.member.roles.cache.find(role => role.id == roleId)) {
-            /**@type {Discord.VoiceChannel} */
-            var voiceChannel = await client.channels.fetch(voiceChannelId)
-            if(!db.isMuted) {
-                voiceChannel.members.forEach(async m => {
-                    m.voice.setMute(true)
-                })
-                msg.reply('channel muted! SHHHHHHHHH!')
-                    .then(msg => {
-                        msg.delete({ timeout: 5000 })
-                    })
-            } else {
-                voiceChannel.members.forEach(async m => {
-                    m.voice.setMute(false)
-                })
-                msg.reply('channel un-muted! Speak!')
-                    .then(msg => {
-                        msg.delete({ timeout: 5000 })
-                    })
+        // if(msg.member.roles.cache.find(r => r.permissions.has('ADMINISTRATOR')) || msg.member.roles.cache.find(role => role.id == roleId)) {
+        //     /**@type {Discord.VoiceChannel} */
+        //     var voiceChannel = await client.channels.fetch(voiceChannelId)
+        //     if(!db.isMuted) {
+        //         voiceChannel.members.forEach(async m => {
+        //             m.voice.setMute(true)
+        //         })
+        //         msg.reply('channel muted! SHHHHHHHHH!')
+        //             .then(msg => {
+        //                 msg.delete({ timeout: 5000 })
+        //             })
+        //     } else {
+        //         voiceChannel.members.forEach(async m => {
+        //             m.voice.setMute(false)
+        //         })
+        //         msg.reply('channel un-muted! Speak!')
+        //             .then(msg => {
+        //                 msg.delete({ timeout: 5000 })
+        //             })
 
-            }
-            db.isMuted = !db.isMuted
-            await redis.set(msg.guild.id, JSON.stringify(db))
-            await msg.delete()
-            redis.quit()
-            return
-        }
+        //     }
+        //     db.isMuted = !db.isMuted
+        //     await redis.set(msg.guild.id, JSON.stringify(db))
+        //     await msg.delete()
+        //     redis.quit()
+        //     return
+        // }
     }
