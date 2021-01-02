461ffb3c885c9a0a4069779eb62d582112005b3a
da-the-dev <leshiktk@me.com>
2021-01-02 22:02:47 +0300

    Moved MessageEmbeds to a separate files

--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js')
+const messages = require('../messages')
 module.exports =
     /**
      * @param {Array<string>} args Command argument
@@ -6,17 +7,5 @@ module.exports =
      * @param {Discord.Client} client Discord client object
     */
     (args, msg, client) => {
-        var help = new Discord.MessageEmbed()
-            .setTitle('Help menu')
-            .setDescription('All AUM commands for admins and others')
-            .addField(`**\`${client.prefix}setup\`**`, `If you need to refer to a step-by-step guide on how to setup the bot on your server, use this command.`)
-            .addField(`**\`${client.prefix}createRoom\`**`, `To create a room to play Among Us and use AUM, type \`${client.prefix}createRoom <name>\`, replacing \`<name>\` with your desired room name. The voicechannel with the same name will be created in "AMONG US ROOMS" shortly.`)
-            .addField(`**\`${client.prefix}amg\`**`, `Now, to mute the room, type \`${client.prefix}amg\` in any text channel. To un-mute the room, type \`${client.prefix}amg\` again. The lobby will be un-muted.`)
-            // .addField(`**${client.prefix}fix**`, `If user left Among Us channel when it was muted, type \`${client.prefix}fix @member\` in any text chat (Example: \`${client.prefix}fix @daym bro\`). This will give them un-mute them.`)
-            .addField(`**\`${client.prefix}delete\`**`, `If you want to remove AUM from your server, type this in any text channel to let it delete all util roles and categories. After that you can safely kick AUM out.`)
-            .addField("**GitHub**", "This bot was written by hand using Node.js and discord.js! Want to see how it works? Checkout my github repo [here](https://github.com/da-the-dev/Among-Us-Muter)")
-            .addField('**Patreon**', "Love this bot? Consider [donating a few dollans](https://www.patreon.com/da_dev) to help this project grow!")
-            .setColor('#b50005')
-            .setFooter('Among Us Muter by da-the-dev', client.user.avatarURL())
-        msg.channel.send(help)
+        msg.channel.send(messages.help(client))
     }
