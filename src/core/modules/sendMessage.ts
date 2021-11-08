d14cdd06abf68b9987c8ea006dd23e09cb8d915f
da-the-dev <leshiktk@me.com>
2021-11-08 22:53:45 +0300

    More catch blocks

--- src/core/modules/sendMessage.ts
+++ src/core/modules/sendMessage.ts
@@ -4,9 +4,9 @@ export default async (guild: Guild, message: MessageEmbed | string) => {
     await guild.channels.fetch()
     const avalibleTextChannels = guild.channels.cache.filter(c => c.type === 'GUILD_TEXT')
     const general = avalibleTextChannels.find(c => c.name === 'general' || c.name === 'main')
-    if (general && general.isText()) typeof message === 'string' ? await general.send({ content: message }) : await general.send({ embeds: [message] })
+    if (general && general.isText()) typeof message === 'string' ? await general.send({ content: message }).catch(e => e) : await general.send({ embeds: [message] }).catch(e => e)
     if (!general) {
         const tc = avalibleTextChannels.random()
-        if (tc?.isText()) typeof message === 'string' ? await tc.send({ content: message }) : await tc.send({ embeds: [message] })
+        if (tc?.isText()) typeof message === 'string' ? await tc.send({ content: message }).catch(e => e) : await tc.send({ embeds: [message] }).catch(e => e)
     }
 }
