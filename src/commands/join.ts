bab568bd50232a10b61abaaf374e1e4a8445d81a
da-the-dev <leshiktk@me.com>
2021-11-08 23:41:07 +0300

    Added /join

---
+++ src/commands/join.ts
@@ -0,0 +1,28 @@
+import { Collection, VoiceChannel } from 'discord.js'
+import BaseCommand from '../core/interfaces/BaseCommand'
+import { amCategory } from '../core/modules/find'
+
+export default {
+    name: 'join',
+    description: 'Join a random lobby',
+    exec: async i => {
+        const guild = i.guild
+        const member = await guild?.members.fetch({ user: i.member.user.id })
+        if (!member?.voice.channel) {
+            await i.reply({
+                content: 'You must be joined to any voicechannel before being moved to any lobby!',
+                ephemeral: true
+            })
+            return
+        }
+        const category = amCategory(guild!)
+        if (!category) return
+        const lobby = (category.children.filter(c => c.isVoice() && c.name !== 'Lobby generator') as Collection<string, VoiceChannel>).random()
+        if (lobby) {
+            await member?.voice.setChannel(lobby)
+            i.reply({ content: 'Lobby found! Moving...', ephemeral: true })
+        } else {
+            i.reply({ content: 'Couldn\'t find a lobby!', ephemeral: true })
+        }
+    }
+} as BaseCommand
